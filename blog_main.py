e3cf09a1732ec96897760a98276c7e336da0ed94
Min-Ho Lee <minho.lee0716@gmail.com>
2021-07-23 05:06:34 +0900

    Error: 404 Not Found 일 때, 에러 핸들링 함수 추가

--- blog_main.py
+++ blog_main.py
@@ -15,17 +15,22 @@ from flask import Flask
 
 app = Flask(__name__)
 
-#if not app.debug:
-#    import logging
-#    from logging.handlers import RotatingFileHandler # handler는 여러개의 종류가 있다.
-#    file_handler = RotatingFileHandler('test_log.log', maxBytes=2000, backupCount=10)
-#    file_handler.setLevel(logging.WARNING) # 어느 단계까지 로깅을 허용할 것인지.
-#    # app.logger.addHandler()에 등록을 해주어야 app.logger로 사용이 가능하다.
-#    app.logger.addHandler(file_handler)
+if not app.debug:
+    import logging
+    from logging.handlers import RotatingFileHandler # handler는 여러개의 종류가 있다.
+    file_handler = RotatingFileHandler('test_log.log', maxBytes=2000, backupCount=10)
+    file_handler.setLevel(logging.DEBUG) # 어느 단계까지 로깅을 허용할 것인지.
+    # app.logger.addHandler()에 등록을 해주어야 app.logger로 사용이 가능하다.
+    app.logger.addHandler(file_handler)
 
-@app.route("/<user_name>")
-def hello(user_name):
-    return "<h1>Hello " + user_name + "</h1>"
+@app.errorhandler(404)
+def page_not_found(info):
+    app.logger.error(info)
+    return "<h1>404 Not Found Test</h1>", 404
+
+@app.route("/test_page")
+def hello():
+    return "<h1>Hello World</h1>"
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port="8080", debug=True)
