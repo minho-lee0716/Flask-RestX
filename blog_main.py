6e4ff30ad39705dd00de599c16d98e5d7fac1968
Min-Ho Lee <minho.lee0716@gmail.com>
2021-07-22 23:59:19 +0900

    [Setting] - Environ에 따른 설정 변경

--- blog_main.py
+++ blog_main.py
@@ -2,3 +2,7 @@ from flask import Flask, jsonify, request, render_template, make_response
 from flask_login import LoginManager, current_user, login_required, login_user, logout_user
 from flask_cors import CORS
 import os
+
+# https 만을 지원하는 기능을 http 에서 테스트할 때 필요한 설정
+os.environ['OAUTHLIB-INSECURE_TRANSPORT'] = '1'
+app = Flask(__name__, static_url_path='/static')
