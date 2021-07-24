82981dc755ee466d657feb5463790ab4d06c1965
Min-Ho Lee <minho.lee0716@gmail.com>
2021-07-24 22:58:19 +0900

    Modify: Desc HTTP Request

--- blog_main.py
+++ blog_main.py
@@ -28,19 +28,19 @@ def page_not_found(info):
     app.logger.error(info)
     return "<h1>404 Not Found Test</h1>", 404
 
-@app.before_first_request
-def before_first_request():
-    print('Flask 실행 후, 첫 요청 때만 실행이 됨')
-
-@app.before_request
-def before_first_request():
-    print('HTTP 요청이 들어올 때마다 실행이 됨')
-
-
-@app.after_request
-def after_request(response):
-    print('HTTP 요청 처리가 끝나고 브라우저에 응답하기 전에 실행')
-    return response
+#@app.before_first_request
+#def before_first_request():
+#    print('Flask 실행 후, 첫 요청 때만 실행이 됨')
+#
+#@app.before_request
+#def before_first_request():
+#    print('HTTP 요청이 들어올 때마다 실행이 됨')
+#
+#
+#@app.after_request
+#def after_request(response):
+#    print('HTTP 요청 처리가 끝나고 브라우저에 응답하기 전에 실행')
+#    return response
 
 @app.route("/hello")
 def hello():
